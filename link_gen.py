36b57d68210118b99b37bb28cb925d6f21832177
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-11 14:34:40 +0530

    added social hq svgs assets

--- link_gen.py
+++ link_gen.py
@@ -1,24 +1,14 @@
 import os
 import yaml
 
-images = [file[:-4] for file in os.listdir('social_media_logos') if file.endswith('.png') ]
 
-with open('social_media_logos/links.yml') as f:
+with open('social_links.yml') as f:
     link_dict = yaml.full_load(f)
 
 
 with open('social.md','w+') as f:
-    f.write('<center>\n\n')
-    for img in images:
-        link = link_dict[img]
-        
-        img_url = f'https://github.com/aahnik/aahnik/blob/master/social_media_logos/{img}.png?raw=true'
-        template = f'<a href = "{link}" > <img src = "{img_url}" alt = "{img}" > </a >'
-        f.write(f'{template}\n')
-
-    f.write('\n\n</center>')
-
-
-
-
+    for img_name,link in link_dict.items():
+        img_url = f'https://github.com/aahnik/aahnik/blob/master/svg_assets/{img}.svg?raw=true'
+        template = f'<a href = "{link}" > <img src = "{img_url}" alt = "{img} width=50" > </a >'
+        f.write(f'{template}\n\n')
 
